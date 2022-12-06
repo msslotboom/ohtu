d406122d4a77fdcc606db3ba09de056a95d27e8b
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-12-07 00:50:55 +0200

    Commit unsaved file

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,14 +2,14 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_points = 0
+        self.player2_points = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.player1_points = self.player1_points + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_points = self.player2_points + 1
 
     def _point_to_text(self, point):
         if point == 0:
@@ -22,33 +22,33 @@ class TennisGame:
             return "Forty"
         
     def _get_score_if_equal(self):
-        if self.m_score1 >= 4:
+        if self.player1_points >= 4:
                 return "Deuce"
         else:
-           return self._point_to_text(self.m_score1) + "-All"
+           return self._point_to_text(self.player1_points) + "-All"
 
     def _get_score_above_four(self):
-        minus_result = self.m_score1 - self. m_score2
+        point_difference = self.player1_points - self. player2_points
 
-        if minus_result == 1:
+        if point_difference == 1:
             score = "Advantage player1"
-        elif minus_result == -1:
+        elif point_difference == -1:
             score = "Advantage player2"
-        elif minus_result >= 2:
+        elif point_difference >= 2:
             score = "Win for player1"
         else:
             score = "Win for player2"
         return score
 
-    def _get_score_if_not_equal_under_four(self):
+    def _get_score_if_not_equal_but_under_four(self):
         score = ""
         temp_score = 0
         for i in range(1, 3):
             if i == 1:
-                temp_score = self.m_score1
+                temp_score = self.player1_points
             else:
                 score = score + "-"
-                temp_score = self.m_score2
+                temp_score = self.player2_points
 
             score = score + self._point_to_text(temp_score)
         return score
@@ -56,12 +56,12 @@ class TennisGame:
     def get_score(self):
         score = ""
 
-        if self.m_score1 == self.m_score2:
+        if self.player1_points == self.player2_points:
             score = self._get_score_if_equal()
 
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
+        elif self.player1_points >= 4 or self.player2_points >= 4:
             score = self._get_score_above_four()
         else:
-            score = self._get_score_if_not_equal_under_four()
+            score = self._get_score_if_not_equal_but_under_four()
 
         return score
